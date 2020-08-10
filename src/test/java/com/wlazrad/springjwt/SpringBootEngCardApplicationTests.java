c1ef76eed84c9c668c75af1fc16fbb0c56745e2e
Wlazrad <radek.wlazly@gmail.com>
2020-08-10 23:21:02 +0200

    rename

--- src/test/java/com/wlazrad/springjwt/SpringBootEngCardApplicationTests.java
+++ src/test/java/com/wlazrad/springjwt/SpringBootEngCardApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootSecurityJwtApplicationTests {
+public class SpringBootEngCardApplicationTests {
 
 	@Test
 	public void contextLoads() {
